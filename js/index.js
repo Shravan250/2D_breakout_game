d183b2be0abe0501ebf1cd18e6e44a92fbce835a
Shravan250 <shravanbobade25@gmail.com>
2025-04-27 00:22:20 +0530

    add ball sprite

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
+  preload,
+  create,
+  update,
+});
+
+let ball;
+
+function preload() {
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  game.stage.backgroundColor = "#eee";
+  game.load.image("ball", "img/ball.png");
+}
+
+function create() {
+  ball = game.add.sprite(50, 50, "ball");
+}
+
+function update() {}
