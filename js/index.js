f48e48b9464c319a7be1813c8c4428c355c66313
Shravan250 <shravanbobade25@gmail.com>
2025-04-27 12:34:51 +0530

    added paddle and collision between ball and paddle

--- js/index.js
+++ js/index.js
@@ -32,17 +32,49 @@ const config = {
 const game = new Phaser.Game(config);
 
 let ball;
+let paddle;
 
 function preload() {
   this.load.image("ball", "img/ball.png");
+  this.load.image("paddle", "img/paddle.png");
 }
 
 function create() {
-  ball = this.physics.add.sprite(240, 160, "ball");
+  // Create paddle
+  paddle = this.physics.add.sprite(
+    this.sys.game.config.width * 0.5,
+    this.sys.game.config.height - 5,
+    "paddle"
+  );
+  paddle.setOrigin(0.5, 1);
+  paddle.setImmovable(true);
+
+  // Create ball
+  ball = this.physics.add.sprite(
+    this.sys.game.config.width * 0.5,
+    this.sys.game.config.height - 50,
+    "ball"
+  );
+  ball.setOrigin(0.5);
   ball.body.setCollideWorldBounds(true);
   ball.body.setBounce(1, 1);
-
   ball.body.setVelocity(150, 150);
+
+  // Add collision between ball and paddle
+  this.physics.add.collider(ball, paddle);
+
+  //Enable pointer input
+  this.input.on(
+    "pointermove",
+    (pointer) => {
+      paddle.x = Phaser.Math.Clamp(
+        pointer.x,
+        paddle.width / 2,
+        this.sys.game.config.width - paddle.width / 2
+      );
+    },
+    this
+  );
 }
 
 function update() {}
